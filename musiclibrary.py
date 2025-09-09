2bb4dae0df9ecdb367b660f68c232e67d6ab29db
deepsk2525 <deepsk2525@gmail.com>
2025-09-09 14:34:37 +0530

    jenny

---
+++ musiclibrary.py
@@ -0,0 +1,4 @@
+music = {
+    "roots" : "https://www.youtube.com/watch?v=e36s_nZCD94&list=RDe36s_nZCD94&start_radio=1",
+    "challa" : "https://www.youtube.com/watch?v=9a4izd3Rvdw&list=RD9a4izd3Rvdw&start_radio=1" ,  
+}
